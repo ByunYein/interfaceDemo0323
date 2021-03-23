fd65c9e86559c00eed0c5cfb05e82c5a3cf3c41c
ByunYein <dpdls311@gmail.com>
2021-03-23 11:35:58 +0900

    implements interface

--- interfacePrograms/src/main/java/com/example/interfaceprograms/BankAccount.java
+++ interfacePrograms/src/main/java/com/example/interfaceprograms/BankAccount.java
@@ -1,6 +1,6 @@
 package com.example.interfaceprograms;
 
-public class BankAccount {
+public class BankAccount implements Measurable  {
     private double balance;
     private int accountnumber;
     private static int LastAssignedNumber = 1000;
@@ -11,7 +11,7 @@ public class BankAccount {
         accountnumber=LastAssignedNumber;
     }
 
-    public double getBalance() {
+    public double getMeasure() {
         return balance;
     }
 }
